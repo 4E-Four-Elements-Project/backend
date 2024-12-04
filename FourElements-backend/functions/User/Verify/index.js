0d9f4ce41c3ef21d9348708aa8a3a51c95dd2f55
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-12-04 13:50:43 +0100

    added comments to verify function

--- FourElements-backend/functions/User/Verify/index.js
+++ FourElements-backend/functions/User/Verify/index.js
@@ -1,36 +1,63 @@
 import responseHandler from '../../../responses/index'
-const {sendResponse, sendError} = responseHandler
 import middy from '@middy/core';
-import auth from '../../../middleware/auth';
-const { verifyToken } = auth;
-import getUserId from '../../../middleware/getUserId';
 import jsonBodyParser from '@middy/http-json-body-parser';
+import validator from '@middy/validator'
+import { transpileSchema } from '@middy/validator/transpile'
+import httpErrorHandler from '@middy/http-error-handler'
+import auth from '../../../middleware/auth';
+import getUserId from '../../../middleware/getUserId';
+
+const {sendResponse, sendError} = responseHandler
+const { verifyToken } = auth;
 
 async function verify(event){
 
+    //Get the username and token from the event body
     const {username, token} = event.body;
 
+    //Error message if either username or token is missing
     if(!username || !token) return sendError(400, "Username and token are required")
 
+    //Check the database to see if user exist in database and save details in variable
     const foundUser = await getUserId(username)
-    // console.log('foundUserResult: ', foundUserResult);
+
+    //Error message if user isn't in database
     if (!foundUser) {
         return sendError(400, "User not found");
     }
-    // const foundUser = foundUserResult.Items[0]
-    console.log('foundUser: ', foundUser);
     
+    //If user is found save user details in variables
     const userId = foundUser.userId.S
     const tokens = foundUser.tokens?.L?.map(item => item.S) || []
     if (!tokens.includes(token)) {
         return sendError(400, "Token not found");
     }
 
+    //Verify that the token is valid
     const verificationResponse = verifyToken(userId, token)
+
+    //Error message if token isn't valid
     if(!verificationResponse.verified) return sendError(400, verificationResponse.message)
     
+    //Success message if token is valid
     return sendResponse('Successfull verification of token')
+}
+
+const schema = {
+    type: 'object',
+    properties: {
+        body: {
+            type: 'object',
+            properties: {
+                username: { type: 'string', minLength: 4 },
+                token: { type: 'string', minLength: 10 },
+            },
+            required: ['username', 'token'],
+        },
+    },
 }
 
 module.exports.handler = middy(verify)
     .use(jsonBodyParser())
+    .use(validator({ eventSchema: transpileSchema(schema) }))
+    .use(httpErrorHandler())
