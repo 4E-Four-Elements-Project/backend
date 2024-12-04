1c62aa4302d61ddb4c17bea8010851b8e5a6c233
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-12-04 12:16:44 +0100

    functioning verify function

--- FourElements-backend/functions/User/Verify/index.js
+++ FourElements-backend/functions/User/Verify/index.js
@@ -4,18 +4,33 @@ import middy from '@middy/core';
 import auth from '../../../middleware/auth';
 const { verifyToken } = auth;
 import getUserId from '../../../middleware/getUserId';
+import jsonBodyParser from '@middy/http-json-body-parser';
 
-async function verify(userId, token){
-    if(!userId || !token) return sendError(400, "Username and token are required")
+async function verify(event){
+
+    const {username, token} = event.body;
+
+    if(!username || !token) return sendError(400, "Username and token are required")
+
+    const foundUser = await getUserId(username)
+    // console.log('foundUserResult: ', foundUserResult);
+    if (!foundUser) {
+        return sendError(400, "User not found");
+    }
+    // const foundUser = foundUserResult.Items[0]
+    console.log('foundUser: ', foundUser);
+    
+    const userId = foundUser.userId.S
+    const tokens = foundUser.tokens?.L?.map(item => item.S) || []
+    if (!tokens.includes(token)) {
+        return sendError(400, "Token not found");
+    }
 
     const verificationResponse = verifyToken(userId, token)
     if(!verificationResponse.verified) return sendError(400, verificationResponse.message)
-    
-    const foundUser = await getUserId(username)
-    if(!foundUser) return sendError(400, "User not found")
-    if(!foundUser.tokens.includes(token)) return sendError(400, "Token not found")
     
     return sendResponse('Successfull verification of token')
 }
 
 module.exports.handler = middy(verify)
+    .use(jsonBodyParser())
